781f5fe138be5f4ca62231311cc620b8b72f7098
kjh5848 <kjh5848@gmail.com>
2025-11-03 10:21:24 +0900

    refactor: list.mustache 수정

--- src/main/java/com/metacoding/spring_oauth/_core/utils/KakaoToken.java
+++ src/main/java/com/metacoding/spring_oauth/_core/utils/KakaoToken.java
@@ -5,14 +5,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.metacoding.spring_oauth.user.KakaoResponse;
 
-@Component
 public class KakaoToken {
 
     @Value("${kakao.client-id}")
